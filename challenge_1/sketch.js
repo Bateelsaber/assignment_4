bd1e0948689ef84896f5f2495ef2eea5b49a751d
Bateelsaber <sabeb234@newschool.edu>
2015-09-27 05:47:39 +0300

    challenge 1 coding

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -7,12 +7,36 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(50, 50, 150);
+	background(100, 200, 255);
+	// draw rectangle
+	fill(50, 200, 90);
+	rect(0, 210, 640, 350);
+	for (var i = 0; i < 40; i++) {
+		drawTarget(random(10, 630), random(140, 340), 40);
+	}
 
-	// set a fill color
-	fill(255, 255, 255);
+	function drawTarget(x, y, size) {
+		// draw stem
+		fill(0, 150, 20);
+		rect(x - 1.5, y, 5, 90);
+		// back green circle
+		fill(0, 195, 0, 90);
+		ellipse(x, y, size - 30, size - 30);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+		// outside ring
+		fill(255, 255, 255, 88);
+		ellipse(x, y, size, size);
+
+		// middle ring
+		fill(255, 255, 255, 88);
+		ellipse(x, y, size - 10, size - 10);
+
+		// inside ring
+		fill(255, 255, 255, 88);
+		ellipse(x, y, size - 20, size - 20);
+
+		// smallest insid ring
+		fill(255, 255, 255, 88);
+		ellipse(x, y, size - 40, size - 40);
+	}
 }
