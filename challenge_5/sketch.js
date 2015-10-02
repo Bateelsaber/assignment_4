828ebe01073cabd9505d50be00914942f484b383
Bateelsaber <sabeb234@newschool.edu>
2015-10-01 23:57:59 -0400

    draw line and add comments

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -7,15 +7,23 @@ function setup() {
 function draw() {
 	background(50, 0, 0);
 	if (mouseIsPressed) {
+		//draw line
+		stroke(255);
+		strokeWeight(8);
+		line(100, 100, mouseX, mouseY);
+		// draw white circles
+		noStroke();
 		fill(255);
 		ellipse(100, 100, 50, 50);
 		ellipse(mouseX, mouseY, 50, 50);
+		//draw blue circles
 		fill(0, 100, 250);
 		ellipse(100, 100, 30, 30);
 		ellipse(mouseX, mouseY, 30, 30);
 
-	} else {
 
+	} else {
+		// reoeat drawing white and blue circles
 		fill(255);
 		ellipse(mouseX, mouseY, 50, 50);
 		fill(0, 100, 250);
