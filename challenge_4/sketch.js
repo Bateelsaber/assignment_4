b4ee025aa8cb49055e3462460714b9e1a0988d0a
Bateelsaber <sabeb234@newschool.edu>
2015-10-02 18:10:29 -0400

    added ball size variable

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,12 +1,14 @@
 var xPosition = [];
 var yPosition = [];
+var BallSize = [];
 
 function setup() {
 	createCanvas(640, 480);
 	noStroke();
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < 20; i++) {
 		xPosition[i] = random(0, 640);
 		yPosition[i] = random(0, 480);
+		BallSize[i] = random(30);
 
 
 	}
@@ -15,15 +17,11 @@ function setup() {
 function draw() {
 	background(150, 0, 0);
 
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < 20; i++) {
 		xPosition[i] = xPosition[i] + random(-4, 4);
 		yPosition[i] = yPosition[i] + random(-4, 4);
+
 		fill(0, 255, 255);
-		ellipse(xPosition[i] + 240, yPosition[i] - 130, 5, 5);
-
-		ellipse(xPosition[i] - 90, yPosition[i] + 180,
-			12, 12);
-
-		ellipse(xPosition[i], yPosition[i], 25, 25);
+		ellipse(xPosition[i], yPosition[i], BallSize[i], BallSize[i]);
 	}
 }
