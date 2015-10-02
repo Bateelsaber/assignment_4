6edb532b2c1350ce5b9287cbb8c07a044d0ffa18
Bateelsaber <sabeb234@newschool.edu>
2015-10-01 23:52:08 -0400

    change value of x for the ellipse

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,17 +1,18 @@
-function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
-	noStroke();
-}
+var position =
+	function setup() {
+		// create a place to draw
+		createCanvas(640, 360);
+		noStroke();
+	}
 
 function draw() {
 	background(50, 0, 0);
 	if (mouseIsPressed) {
 		fill(255);
-		ellipse(position, position, 50, 50);
+		ellipse(100, 100, 50, 50);
 		ellipse(mouseX, mouseY, 50, 50);
 		fill(0, 100, 250);
-		ellipse(position, position, 30, 30);
+		ellipse(100, 100, 30, 30);
 		ellipse(mouseX, mouseY, 30, 30);
 
 	} else {
